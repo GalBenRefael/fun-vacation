2b793a240c56a3f162697a091584aa43ea993b41
GalBenRefael <galbrefael@gmail.com>
2023-06-04 12:09:22 +0300

    order form

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,5 +1,60 @@
+import FormLayout from '../components/FormLayout';
+import Title from '../components/Title';
+
 function Order() {
-    return <>Order</>;
+    return (
+        <>
+            <Title
+                mainText='Order Now'
+                subText='Quickly order a new vacation'
+            />
+
+            <FormLayout>
+                <form action=''>
+                    <div className='row g-3'>
+                        <div className='col-12'>
+                            <label className='form-label'>
+                                Vacation Package
+                            </label>
+                            <select className='form-select'>
+                                <option value=''>Please Select...</option>
+                            </select>
+                        </div>
+                        <div className='col-12'>
+                            <label className='form-label'>Name</label>
+                            <input
+                                className='form-control'
+                                type='text'
+                            ></input>
+                        </div>
+                        <div className='col-12'>
+                            <label className='form-label'>Email</label>
+                            <input
+                                className='form-control'
+                                type='text'
+                            ></input>
+                        </div>
+                        <div className='form-check mt-4'>
+                            <input
+                                className='form-check'
+                                type='checkbox'
+                            ></input>
+                            <label className='form-check-label'>
+                                I agree to terms...
+                            </label>
+                        </div>
+
+                        <button
+                            className='w-100 btn btn-primary btn-lg mt-4'
+                            type='submit'
+                        >
+                            Continue to checkout
+                        </button>
+                    </div>
+                </form>
+            </FormLayout>
+        </>
+    );
 }
 
 export default Order;
